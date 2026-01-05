82d31056dad5345acd4d8e5b87e934d2448ffd23
Louis-Mael Derval <louis-mael.derval@epita.fr>
2026-01-05 16:24:20 +0100

    fix(qnaasm: move): add accept method

--- src/qnaasm/move.py
+++ src/qnaasm/move.py
@@ -1,9 +1,17 @@
+from typing import TYPE_CHECKING
+
 from qnaasm.qnaasm import QNAasm
 
 from utils.position import Position
+
+if TYPE_CHECKING:
+    from qnaasm.visitor import Visitor
 
 
 class Move(QNAasm):
     def __init__(self, start: Position, end: Position):
         self.start = start
         self.end = end
+
+    def accept(self, visitor: "Visitor"):
+        visitor.visit_move(self)
