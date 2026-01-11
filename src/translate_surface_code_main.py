6aca910a242fdd0193d1383655636be56631913c
Louis-Mael Derval <louis-mael.derval@epita.fr>
2026-01-11 12:43:37 +0100

    chore(translate: surface_code): print qubit mapping

--- src/translate_surface_code_main.py
+++ src/translate_surface_code_main.py
@@ -9,8 +9,12 @@ from qnaasm.pretty_printer import PrettyPrinter
 c = Circuit.loadproto("./ghz-corrected.pb")
 
 qubits = create_all_surface_code_mapping_for_circuit(c)
-# for qubit, pos in qubits.items():
-#     print(f"{qubit}: {pos}")
+print("#" * 40)
+print("# Surface Code qubits mapping.")
+print("#" * 40)
+for qubit, pos in qubits.items():
+    print(f"# {qubit}: {pos}")
+print("#" * 40)
 
 instructions = translate(c, qubits)
 
