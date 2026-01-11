91fcf02541390f31065a6f0fdc0686f457666e85
Louis-Mael Derval <louis-mael.derval@epita.fr>
2026-01-11 12:03:10 +0100

    style(qnaasm: pretty_printer): reformat code

--- src/qnaasm/pretty_printer.py
+++ src/qnaasm/pretty_printer.py
@@ -23,47 +23,47 @@ class PrettyPrinter(Visitor):
         self.level = 0
 
     def visit_block(self, e: "Block"):
-        print(' ' * self.level, "{", sep='')
+        print(" " * self.level, "{", sep="")
         self.level += 2
         super().visit_block(e)
         self.level -= 2
-        print(' ' * self.level, "}", sep='')
+        print(" " * self.level, "}", sep="")
 
     def visit_calloc(self, e: "Calloc"):
-        print(' ' * self.level, f"calloc {e.name}[{e.size}]", sep='')
+        print(" " * self.level, f"calloc {e.name}[{e.size}]", sep="")
 
     def visit_conditional(self, e: "Conditional"):
-        print(' ' * self.level, f"if {e.creg} = {e.value} then", sep='')
+        print(" " * self.level, f"if {e.creg} = {e.value} then", sep="")
         self.level += 2
         super().visit_qnaasm(e.instruction)
         self.level -= 2
 
     def visit_gate(self, e: "Gate"):
-        print(' ' * self.level, f"gate {e.name} ", end="", sep='')
+        print(" " * self.level, f"gate {e.name} ", end="", sep="")
         for target in e.targets[:-1]:
-            print(f"{target},", end="", sep='')
-        print(f"{e.targets[-1]}", sep='')
+            print(f"{target},", end="", sep="")
+        print(f"{e.targets[-1]}", sep="")
 
     def visit_measure(self, e: "Measure"):
-        print(' ' * self.level, "measure ", end="", sep='')
+        print(" " * self.level, "measure ", end="", sep="")
         for target in e.targets[:-1]:
-            print(f"{target},", end="", sep='')
-        print(f"{e.targets[-1]} to {e.creg}", sep='')
+            print(f"{target},", end="", sep="")
+        print(f"{e.targets[-1]} to {e.creg}", sep="")
 
     def visit_move(self, e: "Move"):
-        print(' ' * self.level, f"move {e.start} to {e.end}", sep='')
+        print(" " * self.level, f"move {e.start} to {e.end}", sep="")
 
     def visit_qalloc(self, e: "Qalloc"):
-        print(' ' * self.level, "qalloc ", end="", sep='')
+        print(" " * self.level, "qalloc ", end="", sep="")
         for target in e.targets[:-1]:
-            print(f"{target},", end="", sep='')
-        print(f"{e.targets[-1]}", sep='')
+            print(f"{target},", end="", sep="")
+        print(f"{e.targets[-1]}", sep="")
 
     def visit_qfree(self, e: "Qfree"):
-        print(' ' * self.level, "qfree ", end="", sep='')
+        print(" " * self.level, "qfree ", end="", sep="")
         for target in e.targets[:-1]:
-            print(f"{target},", end="", sep='')
-        print(f"{e.targets[-1]}", sep='')
+            print(f"{target},", end="", sep="")
+        print(f"{e.targets[-1]}", sep="")
 
     def visit_qnaasm(self, e: "QNAasm"):
         super().visit_qnaasm(e)
