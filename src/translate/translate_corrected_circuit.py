541859f3602db40ba5107870278820e3c362c0ea
Louis-Mael Derval <louis-mael.derval@epita.fr>
2026-01-18 00:10:31 +0100

    feat(translate): add script to translate a corrected circuit to QNAasm

---
+++ src/translate/translate_corrected_circuit.py
@@ -0,0 +1,30 @@
+from translate.surface_code_mapping import create_all_surface_code_mapping_for_circuit
+from translate.translation import translate
+
+from mimiqcircuits import Circuit
+
+from qnaasm.pretty_printer import PrettyPrinter
+
+import sys
+
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <circuit-corrected.pb>", file=sys.stderr)
+    sys.exit(1)
+
+file = sys.argv[1]
+
+c = Circuit.loadproto(sys.argv[1])
+
+qubits = create_all_surface_code_mapping_for_circuit(c)
+print("======== Surface Code qubits mapping =======", file=sys.stderr)
+for qubit, pos in qubits.items():
+    print(f"  {qubit}: {pos}", file=sys.stderr)
+print("============================================", file=sys.stderr)
+
+instructions = translate(c, qubits)
+
+printer = PrettyPrinter()
+
+for instr in instructions:
+    instr.accept(printer)
