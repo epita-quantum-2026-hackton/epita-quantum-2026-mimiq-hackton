fd4b8ac81caa5ad2de737f13e430024c56b29780
QuruSelt <quentin.sef@gmail.com>
2026-01-19 13:11:57 +0100

    Feat: Check gate size, unparallelize and convert some gates

--- src/InputHandling.jl
+++ src/InputHandling.jl
@@ -4,12 +4,102 @@ using MimiqCircuits
 
 export InputError, validate_circuit
 
+VALID = [
+    GateX(),
+    GateZ(),
+    GateH(),
+    GateT(),
+    GateCX(),
+    GateSWAP()
+]
+
+ALLOWED = [
+    GateY(),
+    GateCY(),
+    GateCZ()
+]
+
 struct InputError <: Exception
     msg::String
 end
 Base.showerror(io::IO, e::InputError) = print(io, e.msg)
 
-function validate_circuit(c::MimiqCircuits.Circuit)
+# Helpers for checking circuit
+function sanitize_parallel(circuit::MimiqCircuits.Circuit)
+    """
+    Decompose only the gates of class Parallel
+    """
+    new_c = MimiqCircuits.Circuit()
+    for i in eachindex(circuit)
+        ins = circuit[i]
+        if getoperation(ins) === MimiqCircuitsBase.Parallel
+            gate = getoperation(getoperation(ins))
+            s = numqubits(ins)
+            for i in 1:s:len(getqubits(inst))
+                push!(new_c, gate, getqubits(inst)[i:i+s]...)
+            end
+        else
+            push!(new_c, ins)
+        end
+    end
+    return new_c
+end
+
+function check_gate_type(circuit::MimiqCircuits.Circuit)
+    """
+    Check we are using gates allowed by the computer
+    Correct some gates into ones allowed by the computer
+    Raise an Exception if the gate cannot be used
+    """
+    new_c = MimiqCircuits.Circuit()
+    for i in eachindex(circuit)
+        ins = circuit[i]
+        if getoperation(ins) in VALID
+            push!(new_c, getoperation(ins), getqubits(ins)...)
+        else 
+            if getoperation(ins) in ALLOWED
+                if getoperation(ins) === GateY()
+                    changeY(new_c,  getqubits(ins)...)
+                end
+                if getoperation(ins) === GateCY()
+                    changeCY(new_c,  getqubits(ins)...)
+                end
+                if getoperation(ins) === GateCZ()
+                    changeCZ(new_c,  getqubits(ins)...)
+                end
+            end
+        end
+    end
+
+    return new_c
+end
+
+function changeY(circuit::MimiqCircuits.Circuit, id::Int)
+    """
+    Decompose a gate Y into authorized gates
+    """
+    push!(circuit, MimiqCircuits.Instruction(MimiqCircuits.GateX(), id))
+    push!(circuit, MimiqCircuits.Instruction(MimiqCircuits.GateZ(), id))
+end
+
+function changeCY(circuit::MimiqCircuits.Circuit, id1::Int,  id2::Int)
+    """
+    Decompose a gate CY into authorized gates
+    """
+    push!(circuit, MimiqCircuits.Instruction(MimiqCircuits.GateCX(), [id1, id2]...))
+    changeCZ(circuit, id1, id2)
+end
+
+function changeCZ(circuit::MimiqCircuits.Circuit, id1::Int,  id2::Int)
+    """
+    Decompose a gate CZ into authorized gates
+    """
+    push!(circuit, MimiqCircuits.Instruction(MimiqCircuits.GateH(), id2))
+    push!(circuit, MimiqCircuits.Instruction(MimiqCircuits.GateCX(), [id1, id2]...))
+    push!(circuit, MimiqCircuits.Instruction(MimiqCircuits.GateH(), id2))
+end
+
+function check_gate_size(c::MimiqCircuits.Circuit)
     for i in 1:length(c)
         inst = c[i]
         qtargets = getqubits(inst)
@@ -19,6 +109,27 @@ function validate_circuit(c::MimiqCircuits.Circuit)
         end
     end
     return true
+end
+
+function filter(circuit::MimiqCircuits.Circuit)
+    try
+        parallel_san_c = sanitize_parallel(circuit)
+        check_gate_size(parallel_san_c)
+        typed_c = check_gate_type(parallel_san_c)
+        return typed_c
+    catch
+        throw(InputError("Unsupported gate at instruction: gate acts on qubits (max 2)."))
+    end
+end
+
+function filter_and_save(circuit::MimiqCircuits.Circuit)
+    try
+        filtered = filter(circuit)
+        saveproto("input_clean.pb", filtered)
+        return true
+    catch
+        throw(InputError("Unexpected error while filtering and saving circuit."))
+    end
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,24 +11,90 @@ struct MockGateN
     include("../src/InputHandling.jl")
     using .InputHandling
 
-    @testset "InputHandling.validate_circuit (MIMIQ integration)" begin
+    @testset "InputHandling (MIMIQ integration)" begin
         # Single-qubit gate
         c1 = MimiqCircuits.Circuit()
         push!(c1, MimiqCircuits.Instruction(MimiqCircuits.GateX(), 1))
-        @test InputHandling.validate_circuit(c1) == true
+        @test InputHandling.check_gate_size(c1) == true
+
+        # Single-qubit gates
+        cx = MimiqCircuits.Circuit()
+        push!(cx, MimiqCircuits.Instruction(MimiqCircuits.GateX(), 1))
+        println("=======================================================================")
+        draw(cx)
+        @test draw(InputHandling.filter(cx)) === nothing
+        println("=======================================================================")
+
+        cz = MimiqCircuits.Circuit()
+        push!(cz, MimiqCircuits.Instruction(MimiqCircuits.GateZ(), 1))
+        println("=======================================================================")
+        draw(cz)
+        @test draw(InputHandling.filter(cz)) === nothing
+        println("=======================================================================")
+
+        ch = MimiqCircuits.Circuit()
+        push!(ch, MimiqCircuits.Instruction(MimiqCircuits.GateH(), 1))
+        println("=======================================================================")
+        draw(ch)
+        @test draw(InputHandling.filter(ch)) === nothing
+        println("=======================================================================")
+
+        c_lots = MimiqCircuits.Circuit()
+        push!(c_lots, MimiqCircuits.Instruction(MimiqCircuits.GateX(), 1))
+        push!(c_lots, MimiqCircuits.Instruction(MimiqCircuits.GateZ(), 1))
+        push!(c_lots, MimiqCircuits.Instruction(MimiqCircuits.GateH(), 1))
+        push!(c_lots, MimiqCircuits.Instruction(MimiqCircuits.GateX(), 1))
+        push!(c_lots, MimiqCircuits.Instruction(MimiqCircuits.GateT(), 1))
+        push!(c_lots, MimiqCircuits.Instruction(MimiqCircuits.GateH(), 1))
+        push!(c_lots, MimiqCircuits.Instruction(MimiqCircuits.GateZ(), 1))
+        push!(c_lots, MimiqCircuits.Instruction(MimiqCircuits.GateX(), 1))
+        println("=======================================================================")
+        draw(c_lots)
+        @test draw(InputHandling.filter(c_lots)) === nothing
+        println("=======================================================================")
+
+        cy = MimiqCircuits.Circuit()
+        push!(cy, MimiqCircuits.Instruction(MimiqCircuits.GateY(), 1))
+        println("=======================================================================")
+        draw(cy)
+        @test draw(InputHandling.filter(cy)) === nothing
+        println("=======================================================================")
 
         # Two-qubit gate (CX)
         c2 = MimiqCircuits.Circuit()
         push!(c2, MimiqCircuits.Instruction(MimiqCircuits.GateCX(), 1, 2))
-        @test InputHandling.validate_circuit(c2) == true
+        @test InputHandling.check_gate_size(c2) == true
+
+        c_cz = MimiqCircuits.Circuit()
+        push!(c_cz, MimiqCircuits.Instruction(MimiqCircuits.GateCZ(), 1, 2))
+        draw(c_cz)
+        println("=======================================================================")
+        @test draw(InputHandling.filter(c_cz)) === nothing
+        println("=======================================================================")
+        @test InputHandling.check_gate_size(c2) == true
+
+        c_cy = MimiqCircuits.Circuit()
+        push!(c_cy, MimiqCircuits.Instruction(MimiqCircuits.GateCY(), 1, 2))
+        draw(c_cy)
+        println("=======================================================================")
+        @test draw(InputHandling.filter(c_cy)) === nothing
+        println("=======================================================================")
 
         # Three-qubit gate should raise InputError
         c3 = MimiqCircuits.Circuit()
         push!(c3, MimiqCircuits.Instruction(MimiqCircuits.GateCCX(), 1, 2, 3))
-        @test_throws InputHandling.InputError InputHandling.validate_circuit(c3)
+        @test_throws InputHandling.InputError InputHandling.check_gate_size(c3)
 
         # Empty circuit is valid
         c4 = MimiqCircuits.Circuit()
-        @test InputHandling.validate_circuit(c4) == true
+        @test InputHandling.check_gate_size(c4) == true
+
+        # Empty circuit is valid
+        c_save = MimiqCircuits.Circuit()
+        push!(c_save, MimiqCircuits.Instruction(MimiqCircuits.GateCY(), 1, 2))
+        @test InputHandling.filter_and_save(c_save) == true
+
+        c = loadproto("input_clean.pb", MimiqCircuits.Circuit)
+        draw(c)
     end
 end
